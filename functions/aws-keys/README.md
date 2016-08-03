5fff379146da189880f6968a96c4588f82acbe51
Stefan Sundin <git@stefansundin.com>
2016-08-03 16:49:38 -0700

    Add NumDays parameter to aws-keys.

--- functions/aws-keys/README.md
+++ functions/aws-keys/README.md
@@ -6,3 +6,6 @@ This function flags users that have not rotated their access keys in a while
 The function also considers two active access keys to be a problem. The purpose
 of having two keys is to safely be able to rotate them, not to use separate keys
 on different machines. [Read the docs](http://docs.aws.amazon.com/general/latest/gr/managing-aws-access-keys.html).
+
+Optional parameter:
+- NumDays: Override the default 90 day period.
--- functions/aws-keys/main.py
+++ functions/aws-keys/main.py
@@ -4,7 +4,7 @@ import boto3, json
 
 APPLICABLE_RESOURCES = ["AWS::IAM::User"]
 
-def evaluate_compliance(user_name):
+def evaluate_compliance(user_name, rule_parameters):
     iam = boto3.client("iam")
     access_keys = iam.list_access_keys(UserName=user_name)
     active_keys = filter(lambda key: key["Status"] == "Active", access_keys["AccessKeyMetadata"])
@@ -17,20 +17,25 @@ def evaluate_compliance(user_name):
             "Annotation": "User has two active keys."
         }
 
-    three_months_ago = datetime.utcnow() - timedelta(days=90)
+    some_time_ago = datetime.utcnow() - timedelta(days=int(rule_parameters["NumDays"]))
     key = active_keys[0]
     create_date = key["CreateDate"].replace(tzinfo=None)
     delta = datetime.utcnow() - create_date
 
     return {
-        "ComplianceType": "NON_COMPLIANT" if create_date < three_months_ago else "COMPLIANT",
+        "ComplianceType": "NON_COMPLIANT" if create_date < some_time_ago else "COMPLIANT",
         "Annotation": "Key is %d days old." % (delta.days)
     }
 
 def lambda_handler(event, context):
     invoking_event = json.loads(event["invokingEvent"])
     configuration_item = invoking_event["configurationItem"]
+    rule_parameters = json.loads(event["ruleParameters"])
+    if "NumDays" not in rule_parameters:
+        rule_parameters["NumDays"] = "90"
+
     print("Evaluating", configuration_item)
+    print("Rule parameters", rule_parameters)
 
     evaluation = {
         "ComplianceResourceType": configuration_item["resourceType"],
@@ -39,7 +44,7 @@ def lambda_handler(event, context):
         "OrderingTimestamp": configuration_item["configurationItemCaptureTime"]
     }
     if configuration_item["configurationItemStatus"] != "ResourceDeleted" and configuration_item["resourceType"] in APPLICABLE_RESOURCES:
-        evaluation.update(evaluate_compliance(configuration_item["resourceName"]))
+        evaluation.update(evaluate_compliance(configuration_item["resourceName"], rule_parameters))
     print("Result", evaluation)
 
     config = boto3.client("config")
@@ -53,5 +58,6 @@ def lambda_handler(event, context):
 # The code below is only for testing, to run it locally, simply run ./main.py
 
 if __name__ == "__main__":
+    rule_parameters = {"NumDays": "30"}
     for user_name in ["john.doe"]:
-        print(user_name, evaluate_compliance(user_name))
+        print(user_name, evaluate_compliance(user_name, rule_parameters))
